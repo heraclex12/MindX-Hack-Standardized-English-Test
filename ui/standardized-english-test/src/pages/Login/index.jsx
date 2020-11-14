60b16622845434377ca5d9bda2bd3049c7f40505
Duy Tran <duy.tran@terralogic.com>
2020-11-14 18:28:38 +0700

    Connect API

--- ui/standardized-english-test/src/pages/Login/index.jsx
+++ ui/standardized-english-test/src/pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { Form, Input, Button, Radio } from 'antd';
 
 import './index.css';
@@ -23,25 +24,13 @@ const Login = (props) => {
     console.log(values);
     const { fullName, password, phone, email, gender } = values;
     const data = {
-      fullName,
-      password,
-      phone,
       email,
-      gender
+      password
     };
-    const url = '';
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      // mode: 'cors', // no-cors, *cors, same-origin
-      // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      // credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      // redirect: 'follow', // manual, *follow, error
-      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    const url = 'http://35.208.221.249:5000/api/login';
+    axios.post(url, data).then((res) => {
+      console.log(res);
+      console.log(res.data);
     });
   };
 
