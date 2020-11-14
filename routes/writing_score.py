320ef9051ff5f110c8141d6f9dd58debcf71353f
unknown <heraclex12@gmail.com>
2020-11-15 04:59:22 +0700

    add relative score

--- routes/writing_score.py
+++ routes/writing_score.py
@@ -11,6 +11,13 @@ from writing.compute_spell_grammar_score import compute_spell_grammar_score
 from writing.encode_doc import encode_doc
 from writing.BasicScoring import basic_score
 from writing.CoherenceScoring import coherence_score
+from writing.RelativeScoring import  prompt_rel_score, prompts
+
+def get_score(logit):
+    min_value = 0
+    max_value = 10
+    overall_score = round(logit * (max_value - min_value) + min_value)
+    return overall_score
 
 
 class WritingScoring(Resource):
@@ -23,6 +30,8 @@ class WritingScoring(Resource):
         spellmar_score, errors = compute_spell_grammar_score(anwser_content)
 
         embeddings = encode_doc(anwser_content, question_id)
+        embeddings = np.expand_dims(embeddings, 0)
         co_score = coherence_score.predict(embeddings)
         se_score = basic_score.predict(embeddings)
-        return {'score': spellmar_score, 'coherence_score': co_score, 'semantic_score': se_score, 'error': errors}, 200
+        re_score = prompt_rel_score.predict(embeddings, prompts[int(question_id)])
+        return {'score': spellmar_score, 'coherence_score': get_score(co_score), 'semantic_score': get_score(se_score), 'relative_score': get_score(re_score), 'error': errors}, 200
