4cd2d0fb00b5dcf817e1c06a36b82fb64a0f234c
lehoanganh298 <lehoanganh29896@gmail.com>
2020-11-15 02:37:03 +0700

    Make request when click submit

--- ui/standardized-english-test/src/pages/DashBoard/Writing.js
+++ ui/standardized-english-test/src/pages/DashBoard/Writing.js
@@ -12,6 +12,7 @@ export default function Writing(props) {
   let { path, url } = useRouteMatch();
   const [prompt, setPrompt] = useState('');
   const [promptID, setPromptID] = useState('0');
+  const [answer,setAnswer] = useState('');
 
   const clickPage = (p, s) => {
     fetch(`http://35.208.221.249:5000/api/get_writing_question/${p}`)
@@ -35,6 +36,34 @@ export default function Writing(props) {
         console.log('Fetch Error :-S', err);
       });
   }
+  const submit = ()=>{
+    fetch('http://35.208.221.249:5000/api/get_writing_score',{
+      method:'POST',
+      body:JSON.stringify({
+        question_id:promptID,
+        question_content:prompt,
+        answer_content:answer
+      })
+    }).then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function (data) {
+          console.log(data)
+          // setPrompt(data.question)
+          // setPromptID(data.id)
+        });
+      }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'center',margin:'10px 0 30px 0' }}>
@@ -46,10 +75,9 @@ export default function Writing(props) {
         {promptID != '0' ? `Prompt ${promptID}` : ""}
       </Title>
       <Typography style={{textAlign:'justify',margin:'20px 30px'}}>{prompt}</Typography>
-      <TextArea rows={4} style={{margin:'20px'}}/>
+      <TextArea rows={4} style={{margin:'20px'}} onChange={(e)=>{setAnswer(e.target.value)}}/>
       <div style={{textAlign:'center', margin:'20px 0px'}}>
-      <Button type="primary" shape="round" size='large' >Submit</Button>
-
+      <Button type="primary" shape="round" size='large' onClick={submit}>Submit</Button>
       </div >
     </div>
   )
