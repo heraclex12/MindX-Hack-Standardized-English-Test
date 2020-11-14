35ac0ff5d19cc5c5fa947f3522206d44f06d2159
unknown <heraclex12@gmail.com>
2020-11-14 13:20:38 +0700

    add flask files

---
+++ models/user.py
@@ -0,0 +1,19 @@
+import base64
+
+class UserProfile:
+    def __init__(self, username, password, gender, email, phone):
+        self.username = username
+        self.password = password
+        self.gender = gender
+        self.email = email
+        self.phone = phone
+        self._hash_pass()
+
+    def __str__(self):
+        print(f'Username={self.username}\nGender={self.gender}\nEmail={self.email}\nPhone={self.phone}\n')
+
+    def get_password(self):
+        return self.password
+
+    def _hash_pass(self):
+        self.password = base64.b64encode(self.password.encode('utf-8'))
